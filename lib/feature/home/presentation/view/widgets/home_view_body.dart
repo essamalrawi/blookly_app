21bed55a0e533640903d36251bbfe1a578695fba
essamalrawi <essamhamdy198@gmail.com>
2024-11-28 22:14:58 +0200

    best seller list view item

--- lib/feature/home/presentation/view/widgets/home_view_body.dart
+++ lib/feature/home/presentation/view/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:blookly_app/core/utils/assets.dart';
 import 'package:blookly_app/core/utils/styles.dart';
 import 'package:blookly_app/feature/home/presentation/view/widgets/custom_app_bar.dart';
 import 'package:blookly_app/feature/home/presentation/view/widgets/featured_list_view.dart';
@@ -22,6 +23,32 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.titleMedium,
           ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            AssetsData.testImage,
+                          ))))),
         ],
       ),
     );
