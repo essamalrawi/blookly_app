0568d77126e25038afe7f5b82f663b3b362f2789
essamalrawi <essamhamdy198@gmail.com>
2024-11-30 21:31:18 +0200

    create book_details_view.dart

---
+++ lib/feature/splash/presentation/views/books_details_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BooksDetails extends StatelessWidget {
+  const BooksDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
