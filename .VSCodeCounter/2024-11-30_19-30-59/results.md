952b50260eb6e7f032cdb14b3eb612e0354423e9
essamalrawi <essamhamdy198@gmail.com>
2024-11-30 19:31:50 +0200

    best seller item part 3

---
+++ .VSCodeCounter/2024-11-30_19-30-59/results.md
@@ -0,0 +1,35 @@
+# Summary
+
+Date : 2024-11-30 19:30:59
+
+Directory c:\\dev\\blookly_app\\lib
+
+Total : 13 files,  318 codes, 0 comments, 44 blanks, all 362 lines
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| Dart | 13 | 318 | 0 | 44 | 362 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 13 | 318 | 0 | 44 | 362 |
+| . (Files) | 2 | 26 | 0 | 6 | 32 |
+| core | 2 | 29 | 0 | 3 | 32 |
+| core\\utils | 2 | 29 | 0 | 3 | 32 |
+| feature | 9 | 263 | 0 | 35 | 298 |
+| feature\\home | 6 | 174 | 0 | 18 | 192 |
+| feature\\home\\presentation | 6 | 174 | 0 | 18 | 192 |
+| feature\\home\\presentation\\view | 6 | 174 | 0 | 18 | 192 |
+| feature\\home\\presentation\\view (Files) | 1 | 11 | 0 | 3 | 14 |
+| feature\\home\\presentation\\view\\widgets | 5 | 163 | 0 | 15 | 178 |
+| feature\\splash | 3 | 89 | 0 | 17 | 106 |
+| feature\\splash\\presentation | 3 | 89 | 0 | 17 | 106 |
+| feature\\splash\\presentation\\views | 3 | 89 | 0 | 17 | 106 |
+| feature\\splash\\presentation\\views (Files) | 1 | 11 | 0 | 3 | 14 |
+| feature\\splash\\presentation\\views\\widgets | 2 | 78 | 0 | 14 | 92 |
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- lib/feature/home/presentation/view/widgets/home_view_body.dart
+++ lib/feature/home/presentation/view/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
-import 'package:blookly_app/core/utils/assets.dart';
 import 'package:blookly_app/core/utils/styles.dart';
+import 'package:blookly_app/feature/home/presentation/view/widgets/best_seller_list_view_item.dart';
 import 'package:blookly_app/feature/home/presentation/view/widgets/custom_app_bar.dart';
 import 'package:blookly_app/feature/home/presentation/view/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
@@ -27,47 +27,6 @@ class HomeViewBody extends StatelessWidget {
             height: 20,
           ),
           BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 120,
-      child: Row(
-        children: [
-          AspectRatio(
-              aspectRatio: 2.5 / 4,
-              child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      image: const DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(
-                            AssetsData.testImage,
-                          ))))),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  'Harry Potter and the Goblet of Fire',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
-                ),
-              )
-            ],
-          )
         ],
       ),
     );
