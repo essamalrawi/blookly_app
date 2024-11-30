fd50e02ea24658af56446701d3af7d9338e89695
essamalrawi <essamhamdy198@gmail.com>
2024-11-30 23:09:53 +0200

    add title and subtitle to details

--- lib/feature/home/presentation/view/widgets/book_details_body.dart
+++ lib/feature/home/presentation/view/widgets/book_details_body.dart
@@ -1,3 +1,4 @@
+import 'package:blookly_app/core/utils/styles.dart';
 import 'package:blookly_app/feature/home/presentation/view/widgets/custom_book_details_app_bar.dart';
 import 'package:blookly_app/feature/home/presentation/view/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +15,23 @@ class BookDetailsBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            padding: EdgeInsets.symmetric(horizontal: width * .20),
             child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text('The Jungle Book', style: Styles.textStyle30),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text('Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500,
+                )),
           ),
         ],
       ),
