d949c5855019d8bc938770a15463023058ddc1f5
essamalrawi <essamhamdy198@gmail.com>
2024-11-30 16:12:21 +0200

    best seller item part 2

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,11 +2,18 @@ import 'package:blookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
   static const textStyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
-
   static const textStyle20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
@@ -16,13 +23,5 @@ abstract class Styles {
     fontSize: 30,
     fontWeight: FontWeight.normal,
     fontFamily: kGtSectraFine,
-  );
-  static const textStyle14 = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.normal,
-  );
-  static const textStyle16 = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.normal,
   );
 }
