e83b13c016c24a8f4422a8e9a6ae4e680ee84b66
essamalrawi <essamhamdy198@gmail.com>
2024-12-02 15:54:08 +0200

    create search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:blookly_app/feature/home/presentation/view/books_details_view.dart';
 import 'package:blookly_app/feature/home/presentation/view/home_view.dart';
+import 'package:blookly_app/feature/search/presentation/views/search_view.dart';
 import 'package:blookly_app/feature/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -19,6 +21,10 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailsView,
         builder: (context, state) => const BooksDetails(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
