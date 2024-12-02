bbdb89bfcdf0e6b7e3636e252b1225cd57099dbb
essamalrawi <essamhamdy198@gmail.com>
2024-12-02 16:19:26 +0200

    remove useless importants

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -1,6 +1,5 @@
 import 'package:blookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({
